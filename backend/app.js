597662a23b2e22ea52adac8abd954ab639569130
Maxim Zhosan <maxim.zhosan@gmail.com>
2022-12-24 21:17:54 +0300

    Feature: Cors handler 35

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
+// const cors = require('cors');
 const { errors } = require('celebrate');
 const { errHandler } = require('./middlewares/err-handler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
@@ -8,27 +8,51 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT = 3000 } = process.env;
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(requestLogger); // подключаем логгер запросов
+mongoose.connect('mongodb://localhost:27017/mestodb');
+
 const allowedCors = [
   'http://mesto-mz.nomoredomains.club',
   'https://mesto-mz.nomoredomains.club',
   'localhost:3000',
 ];
 
-const corsOptions = {
-  origin: allowedCors,
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  optionsSuccessStatus: 204,
-  credentials: true,
-};
+app.use((req, res, next) => {
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  // проверяем, что источник запроса есть среди разрешённых
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
 
-app.use(cors(corsOptions));
+  // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(requestLogger); // подключаем логгер запросов
-mongoose.connect('mongodb://localhost:27017/mestodb');
+  // Если это предварительный запрос, добавляем нужные заголовки
+  if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+  }
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы с этими заголовками
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    // завершаем обработку запроса и возвращаем результат клиенту
+    return res.end();
+  }
+  next();
+});
 
+// const corsOptions = {
+//   origin: allowedCors,
+//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//   optionsSuccessStatus: 204,
+//   credentials: true,
+// };
 
+// app.use(cors(corsOptions));
 
 app.use('/', require('./routes/index'));
 
