34e1cfbeddf369322bbdf321c541ce18642c7846
Maxim Zhosan <maxim.zhosan@gmail.com>
2022-12-24 20:53:23 +0300

    Feature: Cors handler 33

--- backend/app.js
+++ backend/app.js
@@ -13,21 +13,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(requestLogger); // подключаем логгер запросов
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
-// const allowedCors = [
-//   'http://mesto-mz.nomoredomains.club',
-//   'https://mesto-mz.nomoredomains.club',
-//   'localhost:3000',
-// ];
+const allowedCors = [
+  'http://mesto-mz.nomoredomains.club',
+  'https://mesto-mz.nomoredomains.club',
+  'localhost:3000',
+];
 
-// const corsOptions = {
-//   origin: allowedCors,
-//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//   optionsSuccessStatus: 204,
-// };
-
-// app.use(cors(corsOptions));
+const corsOptions = {
+  origin: allowedCors,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  optionsSuccessStatus: 204,
+};
 
 app.use((req, res, next) => {
+  cors(corsOptions);
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
   res.setHeader('Access-Control-Allow-Methods', 'Content-Type', 'Authorization');
