6c168a09975d8d8d25d8c516d3d024f5eb44cabc
Maxim Zhosan <maxim.zhosan@gmail.com>
2022-12-24 22:37:36 +0300

    Feature: Cors handler 45

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -166,7 +166,7 @@ module.exports.login = (req, res, next) => {
                 .cookie('jwt', token, {
                   // token - наш JWT токен, который мы отправляем
                   maxAge: 3600000,
-                  httpOnly: true,
+                  httpOnly: false,
                   secure: false,
                 })
                 .send({ _id: user._id })
