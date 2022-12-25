757967647cfc39f6ed5e1ebdfe4c8d953b14cd84
Maxim Zhosan <maxim.zhosan@gmail.com>
2022-12-26 01:10:44 +0300

    Feature: Cors handler 93

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -13,7 +13,7 @@ module.exports.createCard = (req, res, next) => {
   const owner = req.user._id;
   const { name, link } = req.body;
   Card.create({ name, link, owner })
-    .then((card) => res.status(201).send(card.populate(['owner', 'likes'])))
+    .then((card) => res.status(201).send(card))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         next(new BadRequestError('Переданы некорректные данные при создании карточки'));
@@ -34,7 +34,7 @@ module.exports.deleteCard = (req, res, next) => {
         next(new ForbiddenError('Нельзя удалить чужую карточку'));
       } else {
         card.delete();
-        res.send({ card });
+        res.send(card);
       }
     })
     .catch((err) => {
@@ -79,7 +79,7 @@ module.exports.dislikeCard = (req, res, next) => {
       if (!card) {
         next(new NotFoundError('Карточка с указанным _id не найдена'));
       } else {
-        res.send({ card });
+        res.send(card);
       }
     })
     .catch((err) => {
