fe44ad316ea29e0e939e280e3883bba04ff739de
Maxim Zhosan <maxim.zhosan@gmail.com>
2022-12-25 23:43:09 +0300

    Feature: Cors handler 81

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -14,7 +14,7 @@ module.exports.createCard = (req, res, next) => {
   const owner = req.user._id;
   const { name, link } = req.body;
   Card.create({ name, link, owner })
-    .then((card) => res.status(201).send(card))
+    .then((card) => res.status(201).populate(['owner', 'likes']).send(card))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         next(new BadRequestError('Переданы некорректные данные при создании карточки'));
