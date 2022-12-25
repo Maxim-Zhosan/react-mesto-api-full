1aaa8c5eff5b2a414c363581b2ca26c5ee1ceb8f
Maxim Zhosan <maxim.zhosan@gmail.com>
2022-12-26 00:43:00 +0300

    Feature: Cors handler 85

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -14,7 +14,6 @@ module.exports.createCard = (req, res, next) => {
   const owner = req.user._id;
   const { name, link } = req.body;
   Card.create({ name, link, owner })
-    .populate(['owner'])
     .then((card) => res.status(201).send(card))
     .catch((err) => {
       if (err.name === 'ValidationError') {
