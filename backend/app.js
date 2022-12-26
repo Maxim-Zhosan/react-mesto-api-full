27b2c983fc6bbb6ae394691e3f6afd306ffc6711
Maxim Zhosan <maxim.zhosan@gmail.com>
2022-12-27 02:32:17 +0300

    swsws

--- backend/app.js
+++ backend/app.js
@@ -1,51 +1,19 @@
-require('dotenv').config();
-
-console.log(process.env);
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const { errHandler } = require('./middlewares/err-handler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const { corsHandler } = require('./middlewares/cors-handler');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+app.use((req, res, next) => corsHandler(req, res, next));
+require('dotenv').config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(requestLogger); // подключаем логгер запросов
 mongoose.connect('mongodb://localhost:27017/mestodb');
-
-const allowedCors = [
-  'http://mesto-mz.nomoredomains.club',
-  'https://mesto-mz.nomoredomains.club',
-  'localhost:3000',
-];
-
-app.use((req, res, next) => {
-  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
-  // проверяем, что источник запроса есть среди разрешённых
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
-  }
-  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
-
-  // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-
-  // Если это предварительный запрос, добавляем нужные заголовки
-  if (method === 'OPTIONS') {
-    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-  }
-  const requestHeaders = req.headers['access-control-request-headers'];
-  if (method === 'OPTIONS') {
-    // разрешаем кросс-доменные запросы с этими заголовками
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.end(); // завершаем обработку запроса и возвращаем результат клиенту
-  }
-  next();
-});
 
 app.use('/', require('./routes/index'));
 
