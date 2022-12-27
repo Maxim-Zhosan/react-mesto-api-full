175343c8b575c4ac0612cbd212ac4c38d989c21d
Maxim Zhosan <maxim.zhosan@gmail.com>
2022-12-27 22:57:12 +0300

    Feature: rateLimit

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-
+const rateLimit = require('express-rate-limit');
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
@@ -9,6 +9,15 @@ const { corsHandler } = require('./middlewares/cors-handler');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+app.use(limiter); // Apply the rate limiting middleware to all requests
 app.use((req, res, next) => corsHandler(req, res, next));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
